4b9f5f14ced8b6678d62149611661e32bc724181
Elysia <11175449+jc2915448593@user.noreply.gitee.com>
2026-05-07 21:49:51 +0800

    Revert "merge remote master and local rag updates"
    This reverts commit 6c5a3272037e7814632ebfa898733296ff0af9c6, reversing
    changes made to d72e3d3420ac912005ff72bbc74599fe3fdcda52.

--- 内存向量存储.py
+++
@@ -1,4 +0,0 @@
-from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_community.embeddings import DashScopeEmbeddings
-from langchain_chroma import Chroma
-vector_store = InMemoryVectorStore()
